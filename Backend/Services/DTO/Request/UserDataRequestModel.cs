64ecf424575bc3495e97c3b66f081185a148f7ee
shrikant.chaudhari1403 <Shrikant.chaudhari1403@gmail.com>
2024-08-31 13:30:45 +0530

    backend code

--- Backend/Services/DTO/Request/UserDataRequestModel.cs
+++ Backend/Services/DTO/Request/UserDataRequestModel.cs
@@ -8,10 +8,10 @@ namespace Services.DTO.Request
 {
     public  class UserDataRequestModel
     {
-        public int? Id { get; set; } 
+      //  public int? Id { get; set; } 
         public string? Email { get; set; }
         public string? Password { get; set; }
-        public string? DisplayName { get; set; }
-        public string? Token { get; set; }
+       // public string? DisplayName { get; set; }
+       // public string? Token { get; set; }
     }
 }
