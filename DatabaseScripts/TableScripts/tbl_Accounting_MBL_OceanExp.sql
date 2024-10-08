2ae891d36d55f3585023e7770ea86e318d9dd0fa
dattatrayajshinde <dattatrayajshinde@gmail.com>
2024-10-08 00:18:25 -0400

    added other script

---
+++ DatabaseScripts/TableScripts/tbl_Accounting_MBL_OceanExp.sql
@@ -0,0 +1,18 @@
+Create Table tbl_Accounting_MBL_OceanExp (
+MBL_NO					VARCHAR(20),	
+Reference_No            VARCHAR(20),
+Party                   VARCHAR(50),
+Revenue                 DECIMAL(10,2),
+Cost                    DECIMAL(10,2),
+Balance                 DECIMAL(10,2),
+[Status]                VARCHAR(20),
+[Date]                   DATE,
+Email                   VARCHAR(100),
+[Action]                VARCHAR(50),
+Total_Amount            DECIMAL(10,2),
+Total_Profit            DECIMAL(10,2),
+Amount                  DECIMAL(10,2),
+Profit_Percentage       DECIMAL(5,2),
+Profit_Margin           VARCHAR(20)
+)
+
---
+++ DatabaseScripts/TableScripts/tbl_Pierpass_AP_MBL_OceanExp.sql
@@ -0,0 +1,9 @@
+
+Create TABLE dbo.tbl_Pierpass_AP_MBL_OceanExp (
+MBL_NO					VARCHAR(20),
+Vendor                VARCHAR(200)    NOT NULL,
+Billing_Code          VARCHAR(100)    NOT NULL,
+Charges_Amount1       DECIMAL(10,2),
+Charges_Amount        DECIMAL(10,2),
+[Description]          VARCHAR(500)  
+)
